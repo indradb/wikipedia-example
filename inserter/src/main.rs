203ef10d839f6f01ed008393fae5175d5bb5f690
Yusuf Simonson <simonson@gmail.com>
2020-11-27 11:19:08 -0500

    Fixed article insertion

--- inserter/src/main.rs
+++ inserter/src/main.rs
@@ -41,74 +41,77 @@ async fn build_client(spawner: &LocalSpawner) -> Result<service::Client, CapnpEr
     Ok(client)
 }
 
-async fn insert_articles(client: &service::Client, f: &File, progress: &mut ProgressBar<Stdout>) -> Result<HashMap<String, Uuid>, Box<dyn Error>> {
+async fn insert_articles(client: &service::Client, f: &File, line_count: u64) -> Result<HashMap<String, Uuid>, Box<dyn Error>> {
     let mut uuids = HashMap::<String, Uuid>::new();
-    
+
     let mut params = Params::new();
     let hasher = params.hash_length(16);
 
-    let mut req = client.bulk_insert_request();
-    let mut req_items = req.get().init_items(REQUEST_BUFFER_SIZE);
-    let mut req_index = 0u32;
+    let mut reading_progress = ProgressBar::new(line_count);
+    reading_progress.message("reading articles: ");
 
     for line in BufReader::new(f).lines() {
-        progress.inc();
-
         let mut line = line?;
         if line.starts_with("\t") {
             line = line[1..].to_string();
         }
 
-        if uuids.contains_key(&line) {
-            continue;
+        if !uuids.contains_key(&line) {
+            let uuid = Uuid::from_slice(hasher.hash(line.as_bytes()).as_bytes())?;
+            uuids.insert(line.clone(), uuid);
         }
 
-        let uuid = Uuid::from_slice(hasher.hash(line.as_bytes()).as_bytes())?;
-
-        uuids.insert(line.clone(), uuid);
-
-        {
-            let req_item = req_items.reborrow().get(req_index);
-            let mut builder = req_item.init_vertex().get_vertex()?;
-            builder.set_id(uuid.as_bytes());
-            builder.set_t("article");
-        }
-        {
-            let req_item = req_items.reborrow().get(req_index + 1);
-            let mut builder = req_item.init_vertex_property();
-            builder.set_id(uuid.as_bytes());
-            builder.set_name("name");
-            builder.set_value(&JsonValue::String(line).to_string());
-        }
-
-        req_index += 2;
-
-        if req_index >= REQUEST_BUFFER_SIZE {
-            let res = req.send().promise.await?;
-            res.get()?;
-            req = client.bulk_insert_request();
-            req_items = req.get().init_items(REQUEST_BUFFER_SIZE);
-            req_index = 0;
-        }
+        reading_progress.inc();
     }
 
-    if req_index > 0 {
+    reading_progress.finish();
+    let mut indexing_progress = ProgressBar::new((uuids.len() / (REQUEST_BUFFER_SIZE as usize)) as u64);
+    indexing_progress.message("indexing articles: ");
+
+    let articles_iter: Vec<(&String, &Uuid)> = uuids.iter().collect();
+    for articles_chunk in articles_iter.chunks(REQUEST_BUFFER_SIZE as usize) {
+        let mut req = client.bulk_insert_request();
+        let mut req_items = req.get().init_items((articles_chunk.len() * 2) as u32);
+        let mut req_index = 0u32;
+
+        for (article_name, article_uuid) in articles_chunk {
+            {
+                let req_item = req_items.reborrow().get(req_index);
+                let mut builder = req_item.init_vertex().get_vertex()?;
+                builder.set_id(article_uuid.as_bytes());
+                builder.set_t("article");
+            }
+            {
+                let req_item = req_items.reborrow().get(req_index + 1);
+                let mut builder = req_item.init_vertex_property();
+                builder.set_id(article_uuid.as_bytes());
+                builder.set_name("name");
+                builder.set_value(&JsonValue::String(article_name.to_string()).to_string());
+            }
+
+            req_index += 2;
+        }
+
         let res = req.send().promise.await?;
         res.get()?;
+        indexing_progress.inc();
     }
 
+    indexing_progress.finish();
     Ok(uuids)
 }
 
-async fn insert_links(client: &service::Client, f: &File, uuids: HashMap<String, Uuid>, progress: &mut ProgressBar<Stdout>) -> Result<(), Box<dyn Error>> {
+async fn insert_links(client: &service::Client, f: &File, uuids: HashMap<String, Uuid>, line_count: u64) -> Result<(), Box<dyn Error>> {
     let mut src_uuid: Option<Uuid> = None;
+    
     let mut req = client.bulk_insert_request();
     let mut req_items = req.get().init_items(REQUEST_BUFFER_SIZE);
     let mut req_index = 0u32;
 
-    for line in BufReader::new(f).lines() {
-        progress.inc();
+    let mut indexing_progress = ProgressBar::new(line_count);
+    indexing_progress.message("indexing links: ");
 
+    for line in BufReader::new(f).lines() {
         let line = line?;
         if line.starts_with("\t") {
             let dst_uuid = uuids[&line[1..]];
@@ -131,6 +134,8 @@ async fn insert_links(client: &service::Client, f: &File, uuids: HashMap<String,
         } else {
             src_uuid = Some(uuids[&line]);
         }
+
+        indexing_progress.inc();
     }
 
     if req_index > 0 {
@@ -138,6 +143,7 @@ async fn insert_links(client: &service::Client, f: &File, uuids: HashMap<String,
         res.get()?;
     }
 
+    indexing_progress.finish();
     Ok(())
 }
 
@@ -148,19 +154,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let client = exec.run_until(build_client(&spawner))?;
     
     let mut f = File::open("../data/links.txt")?;
-    let line_count = BufReader::new(&f).lines().count();
+    let line_count = BufReader::new(&f).lines().count() as u64;
 
     f.seek(SeekFrom::Start(0))?;
-    let mut article_progress = ProgressBar::new(line_count as u64);
-    article_progress.message("inserting articles: ");
-    let uuids = exec.run_until(insert_articles(&client, &f, &mut article_progress))?;
-    article_progress.finish();
-
+    let uuids = exec.run_until(insert_articles(&client, &f, line_count))?;
     f.seek(SeekFrom::Start(0))?;
-    let mut link_progress = ProgressBar::new(line_count as u64);
-    link_progress.message("inserting links: ");
-    exec.run_until(insert_links(&client, &f, uuids, &mut link_progress))?;
-    link_progress.finish();
+    exec.run_until(insert_links(&client, &f, uuids, line_count))?;
 
     Ok(())
 }
