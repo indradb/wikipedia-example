76d35648245eb6bad5d2d2a9fcfd39de54dedc0b
Yusuf Simonson <simonson@gmail.com>
2022-01-03 14:03:25 -0500

    Removed old todo

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -5,7 +5,6 @@ use indradb_proto as proto;
 
 pub async fn run(mut client: proto::Client) -> Result<(), Box<dyn StdError>> {
     let start_time = Instant::now();
-    // TODO: drop to_string once generics are employed
     client.execute_plugin("centrality", serde_json::Value::Null).await?;
     println!("finished in {} seconds", start_time.elapsed().as_secs());
     Ok(())
