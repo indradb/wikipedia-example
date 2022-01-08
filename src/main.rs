d722344d043b6b3eed4ba24fdf5cf3fa2c71539e
Yusuf Simonson <simonson@gmail.com>
2022-01-08 14:21:01 -0500

    Improvements to the CLI.
    * Stop randomizing the port. This was nice to have, but it suppressed all stdout/stderr output from IndraDB.
    * Make database server a shared argument.

--- src/main.rs
+++ src/main.rs
@@ -9,34 +9,25 @@ mod indexer;
 use std::convert::TryInto;
 use std::error::Error as StdError;
 use std::ffi::OsStr;
-use std::io::{BufRead, BufReader};
-use std::process::{Child, Command, Stdio};
+use std::process::{Child, Command};
 
 use clap::{App, Arg, SubCommand};
-use indradb_proto as proto;
-use tonic::transport::Endpoint;
+use tokio::time::{sleep, Duration};
 
 pub struct Server {
     child: Child,
-    address: String,
 }
 
 impl Server {
     pub fn start(database_path: &OsStr) -> Result<Self, Box<dyn StdError>> {
-        let mut child = Command::new("indradb/target/release/indradb-server")
+        let child = Command::new("indradb/target/release/indradb-server")
             .args(&[
-                OsStr::new("--address"),
-                OsStr::new("127.0.0.1:0"),
                 OsStr::new("rocksdb"),
                 database_path,
             ])
             .env("RUST_BACKTRACE", "1")
-            .stdout(Stdio::piped())
             .spawn()?;
-
-        let mut lines = BufReader::new(child.stdout.take().unwrap()).lines();
-        let address = lines.next().unwrap()?;
-        Ok(Server { child, address })
+        Ok(Server { child })
     }
 }
 
@@ -49,19 +40,40 @@ impl Drop for Server {
     }
 }
 
+async fn get_client() -> Result<indradb_proto::Client, indradb_proto::ClientError> {
+    let mut client = indradb_proto::Client::new("grpc://127.0.0.1:27615".try_into().unwrap()).await?;
+    client.ping().await?;
+    Ok(client)
+}
+
+async fn get_client_retrying() -> Result<indradb_proto::Client, indradb_proto::ClientError> {
+    let mut retry_count = 10u8;
+    let mut last_err = Option::<indradb_proto::ClientError>::None;
+
+    while retry_count > 0 {
+        match get_client().await {
+            Ok(client) => return Ok(client),
+            Err(err) => {
+                last_err = Some(err);
+                if retry_count == 0 {
+                    break;
+                } else {
+                    sleep(Duration::from_secs(1)).await;
+                    retry_count -= 1;
+                }
+            }
+        }
+    }
+
+    Err(last_err.unwrap())
+}
+
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn StdError>> {
     let archive_arg = Arg::with_name("ARCHIVE_PATH")
         .help("path to the wikipedia dataset archive")
         .long("archive-path")
         .value_name("ARCHIVE_PATH")
-        .required(true)
-        .takes_value(true);
-
-    let datastore_arg = Arg::with_name("DATABASE_PATH")
-        .help("path for storing the IndraDB results")
-        .long("database-path")
-        .value_name("DATABASE_PATH")
         .required(true)
         .takes_value(true);
 
@@ -74,23 +86,27 @@ pub async fn main() -> Result<(), Box<dyn StdError>> {
 
     let matches = App::new("IndraDB wikipedia example")
         .about("demonstrates IndraDB with the wikipedia dataset")
-        .subcommand(SubCommand::with_name("index").arg(&archive_arg).arg(&datastore_arg))
-        .subcommand(SubCommand::with_name("explore").arg(&datastore_arg).arg(&port_arg))
+        .arg(
+            Arg::with_name("DATABASE_PATH")
+                .help("path for storing the IndraDB results")
+                .long("database-path")
+                .value_name("DATABASE_PATH")
+                .required(true)
+                .takes_value(true),
+        )
+        .subcommand(SubCommand::with_name("index").arg(&archive_arg))
+        .subcommand(SubCommand::with_name("explore").arg(&port_arg))
         .get_matches();
+
+    let database_path = matches.value_of_os("DATABASE_PATH").unwrap();
+    let _server = Server::start(database_path)?;
+    let client = get_client_retrying().await.unwrap();
 
     if let Some(matches) = matches.subcommand_matches("index") {
         let archive_path = matches.value_of_os("ARCHIVE_PATH").unwrap();
-        let database_path = matches.value_of_os("DATABASE_PATH").unwrap();
-        let server = Server::start(database_path)?;
-        let endpoint: Endpoint = server.address.clone().try_into()?;
-        let client = proto::Client::new(endpoint).await?;
         indexer::run(client, archive_path).await
     } else if let Some(matches) = matches.subcommand_matches("explore") {
-        let database_path = matches.value_of_os("DATABASE_PATH").unwrap();
         let port = value_t!(matches.value_of("PORT"), u16).unwrap_or_else(|err| err.exit());
-        let server = Server::start(database_path)?;
-        let endpoint: Endpoint = server.address.clone().try_into()?;
-        let client = proto::Client::new(endpoint).await?;
         explorer::run(client, port).await
     } else {
         panic!("no subcommand specified");
